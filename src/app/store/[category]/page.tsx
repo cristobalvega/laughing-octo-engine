2a887816616ca13d06ba9d27c680453a6d6e7ea9
Cristobal Vega <cristobalvega96@gmail.com>
2024-05-14 19:17:57 -0700

    Removed log

--- src/app/store/[category]/page.tsx
+++ src/app/store/[category]/page.tsx
@@ -6,7 +6,6 @@ interface CategoryProps {
 
 export default function Category(props: CategoryProps) {
     const { category } = props.params;
-    console.log('props', props);
     return (
         <h1>Categoría dinamica: {category} </h1>
     )
